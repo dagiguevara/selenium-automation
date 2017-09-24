fe9ca90dbe8329b84a519563d0117fea7441a64f
Donaldo Guevara <dagiguevara@gmail.com>
2017-09-24 14:00:00 -0600

    adding new reports

--- src/test/java/Resources/extentFactory.java
+++ src/test/java/Resources/extentFactory.java
@@ -7,10 +7,10 @@ import com.relevantcodes.extentreports.ExtentReports;
 public class extentFactory {
 	public static ExtentReports getInstance() {
 		ExtentReports extent;
-		String Path = "//Users//donaldguevara//Desktop//reports.html";
+		String Path = "//Users//donaldguevara//Documents//GitHub//zopa-selenium-automation//Reports//reports.html";
 		extent = new ExtentReports(Path, false);
 		extent
-	     .addSystemInfo("Selenium Version", "2.52")
+	     .addSystemInfo("Selenium Version", "2.53.1")
 	     .addSystemInfo("Platform", "Mac");
 
 		return extent;
--- src/test/java/com/zopa/Tests/SignUpFlow.java
+++ src/test/java/com/zopa/Tests/SignUpFlow.java
@@ -10,24 +10,24 @@ import org.openqa.selenium.support.PageFactory;
 
 
 
-public class SIgnUpFlow extends Init {
+public class SignUpFlow extends Init {
 	
 	
 	
 @Test
-  public void SignUpFlow() throws InterruptedException {
+  public void SignUpFlow(){
 	
 	report = extentFactory.getInstance();
 	test = report.startTest("Sign Up Flow Happy Path");
 	SignUpPage SignUp =  PageFactory.initElements(driver, SignUpPage.class);
 	driver.findElement(By.id("member_email")).sendKeys(genData.generateEmail(30));
-	test.log(LogStatus.PASS, "Validate Email");
+	test.log(LogStatus.PASS, "Validaxte Email PASSED");
 	SignUp.PersonalInformation();
-	test.log(LogStatus.PASS, "Personal Information");
+	test.log(LogStatus.PASS, "Personal Information COMPLETE");
 	SignUp.AddressHistory();
-	test.log(LogStatus.PASS, "Address History");
+	test.log(LogStatus.PASS, "Address History COMPLETE");
 	SignUp.FinantialDetails();
-	test.log(LogStatus.PASS, "Finantial Details");
+	test.log(LogStatus.PASS, "Finantial Details COMPLETE");
 	SignUp.submit();
 	test.log(LogStatus.PASS, "Sending Information to a file.txt");
 
