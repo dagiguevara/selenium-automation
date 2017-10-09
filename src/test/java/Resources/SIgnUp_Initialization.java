0f3442b02ca791ff1fb44b1f3d6f7a0b15ef8b23
Donaldo Guevara <dagiguevara@gmail.com>
2017-10-09 13:32:19 -0600

    fixing chrome node bug

--- src/test/java/Resources/SIgnUp_Initialization.java
+++ src/test/java/Resources/SIgnUp_Initialization.java
@@ -7,32 +7,36 @@ import com.relevantcodes.extentreports.LogStatus;
 import com.zopa.Pages.HomePage;
 import com.zopa.Pages.LoansPage;
 import Resources.screenshots;
-
 import org.testng.annotations.BeforeMethod;
-
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 
 public class SIgnUp_Initialization {
 	
-	public WebDriver driver; 
+	public RemoteWebDriver driver; 
 	public ExtentReports report;
 	public ExtentTest test;
 	public GenerateData genData = new GenerateData();
  
   @BeforeMethod
-  public void beforeMethod() throws InterruptedException {
-	     System.setProperty("webdriver.chrome.driver", "/Users/donaldguevara/Desktop/chromedriver/");
-		 driver = new ChromeDriver();
+  public void beforeMethod() throws InterruptedException, MalformedURLException {
+	     
+	     DesiredCapabilities capability = DesiredCapabilities.chrome();
+	     driver = new RemoteWebDriver (new URL ("http://ec2-34-235-112-25.compute-1.amazonaws.com:4446/wd/hub"), capability);
 		 driver.get("http://www.zopa.com");
-		 driver.manage().window().maximize();
+		 driver.manage().window().setSize(new Dimension(1600, 900));
 		 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);	 
 		 driver.findElement(By.xpath("//*[@id=\"footer\"]/div[3]/p/a[2]")).click();
 		 HomePage home =  PageFactory.initElements(driver, HomePage.class); 
